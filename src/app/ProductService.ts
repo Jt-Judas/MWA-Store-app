5c21addc28045f2b8bd33f5acb2a0873d3a663d7
Jt-Judas <judastadeo@ymail.com>
2017-12-08 22:14:57 -0600

    Made changes to the ProductService class

--- src/app/ProductService.ts
+++ src/app/ProductService.ts
@@ -1,19 +1,18 @@
 import { Product } from "./Product";
 import { Injectable } from "@angular/core";
-import { Observable } from 'rxjs/Observable';
-import { of } from 'rxjs/observable/of';
+import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class ProductService {
-
-     products: Product[];
+  
+    products: Product[];
 
     getProducts() {
-       this.products  = [new Product("1", "iPhone", 450, "iPhone desc"), new Product('2', 'iPad', 270, 'iPad desc'), new Product('3', 'Apple Tv', 300, 'Apple TV desc')];
-        return of(this.products);
+        this.products = [new Product("1", "iPhone", 450, "iPhone desc"), new Product('2', 'iPad', 270, 'iPad desc'), new Product('3', 'Apple Tv', 300, 'Apple TV desc')];
+        return Observable.of(this.products);
     }
 
-    deleteProduct(product:Product){
+    deleteProduct(product: Product) {
         this.products.splice(this.products.indexOf(product), 1);
     }
 }
