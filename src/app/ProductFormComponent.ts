5375f17dcd0d85120ed6bcbbdae9291345674a0c
Jt-Judas <judastadeo@ymail.com>
2017-12-14 14:05:10 -0600

    added validation to the reactive forms. Tested on both Observable and Promise

--- src/app/ProductFormComponent.ts
+++ src/app/ProductFormComponent.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { ProductService } from './ProductService';
+import { ProductValidators } from './product-validators';
+import { Validators } from '@angular/forms';
 
 @Component({
     selector: 'product-form',
@@ -12,12 +14,16 @@ export class ProductFormComponent {
     constructor(private productService: ProductService) { }
 
     ProductForm = new FormGroup({
-        name: new FormControl(),
+        name: new FormControl('', [Validators.required], [ProductValidators.isUniqueByObservable]),
         price: new FormControl(),
         description: new FormControl()
     });
 
+    get name(){
+    return this.ProductForm.get('name');
+    }
+
     submit() {
-        this.productService.addProduct(this.ProductForm.get('name').value, this.ProductForm.get('price').value, this.ProductForm.get('description').value);
+                this.productService.addProduct(this.ProductForm.get('name').value, this.ProductForm.get('price').value, this.ProductForm.get('description').value);
     }
 }
